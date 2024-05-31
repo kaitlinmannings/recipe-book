43172d3137ab5ff93f1aa4ce190a2d44cc7396ac
Kaitlin Mannings <dmannings@udallas.edu>
2024-05-31 15:35:24 -0500

    updates to recupe + added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('cook-mode').addEventListener('click', function() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+});
